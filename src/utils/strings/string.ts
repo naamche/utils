19d1c6ee4155b5d53311d378810fbde1485cfaba
Krishna Upadhyay <krishnaupadhyay1997@gmail.com>
2023-07-04 08:51:26 +0545

    fix(strings): use proper docs

--- src/utils/strings/string.ts
+++ src/utils/strings/string.ts
@@ -2,6 +2,7 @@ import { FormatStringOptions } from './string.type';
 
 /**
  * Formats a string based on the specified options.
+ * Use a custom callback if necessary.
  * @param str The string to format.
  * @param options The options to use when formatting the string.
  * @returns The formatted string.
