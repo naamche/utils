cd4bf53fc75d271684594d75b0c0ab445d9f5777
Krishna Upadhyay <krishnaupadhyay1997@gmail.com>
2023-07-04 06:32:19 +0545

    fix(core): remove external from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -12,24 +12,36 @@ export default [
     input: 'src/index.ts',
     output: [
       {
+        file: 'dist/index.min.js',
+        format: 'umd',
+        sourcemap: true,
+        name: 'NaamcheUtils',
+        exports: 'named',
+      },
+      {
         file: packageJson.main,
         format: 'cjs',
         sourcemap: true,
+        exports: 'named',
       },
       {
         file: packageJson.module,
         format: 'esm',
         sourcemap: true,
+        exports: 'named',
       },
     ],
     plugins: [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/__tests__', '**/*.test.ts'],
+      }),
       terser(),
     ],
-    external: ['react', 'react-dom', 'styled-components'],
+    external: [],
   },
   {
     input: 'src/index.ts',
